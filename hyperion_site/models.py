63a0d1bc257a5d5d01d166342d8368cb5eaea31a
Hongxia <hongxia.zhong@gmail.com>
2013-03-05 13:32:33 -0800

    Added CorporateProfile, Equipments, Listings, Requirements models. Next step: syncdb to validate.

--- hyperion_site/models.py
+++ hyperion_site/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from jsonfield import JSONField
 
 '''
 Phone
@@ -123,13 +124,13 @@ Contact Information
 class ContactProfile(models.Model):
 	is_active = models.BooleanField(default=False)
 	first_name = models.CharField(_('First Name'), max_length=30, blank=True)
-	last_name = models.CharField(_('Last Name'), max_length=30, blank=True)	
+	last_name = models.CharField(_('Last Name'), max_length=30, blank=True)
+	corporate = models.ForeignKey(_('Employer'), CorporateProfile, blank=True)
 	title = models.CharField(_('Title'), max_length=20, blank=True)
 	email = models.EmailField(_('E-mail'), blank=True)
 	buy = models.BooleanField(_('Buys'), default=False)
 	sell = models.BooleanField(_('Sells'), default=False)
 	# expertise
-	# company
 
 	def get_full_name(self):        
         return '%s %s' % (self.first_name, self.last_name)
@@ -166,14 +167,74 @@ class CorporateProfile(models.Model):
 	region = models.CharField(_('Region'), max_length=10, choices=REGION_CHOICES, default='OTHER')
 	corporate_type = models.CharField(_('Type'), max_length=10, choices=CORPORATE_TYPE_CHOICES, default='OTHER')
 	website = models.URLField(_('Website'), max_length=200)
-	description = models.TextField(_('Description'))
+	description = models.TextField(_('Description'), blank=True)
 
 	def __unicode__(self):
 		return self.name
 
 ##########################################################################################
-class AbstractEquipment(models.Model):
-	
+'''
+Equipment Core + Profile
+'''
+class EquipmentCore(models.Model):
+	make = models.CharField(_('Make'), max_length=50, blank=True)
+	model = models.CharField(_('Model'), max_length=50, blank=True)
+	year = models.CharField(_('Year'), max_length=4, blank=True)
+	description = models.TextField(_('Description'), blank=True)
 
-class 
+	def __unicode__(self):
+		return 'hello' # TODO
+
+class EquipmentProfile(models.Model):
+	is_active = models.BooleanField(default=False)
+	core = models.ForeignKey(_('Make & Model'), EquipmentCore)
+	corporate = models.ForeignKey(_('Property of'), CorporateProfile, blank=True)
+	contact = models.ForeignKey(_('Contact'), ContactProfile, blank=True) # can only be owned by 1 contact
+	manufacture_date = models.DateField(_('Manufacture Date'), auto_now=False, auto_now_add=False, blank=True)
+	description = models.TextField(_('Description'), blank=True)
+
+	def __unicode__(self):
+		return 'hello' # TODO
+
+##########################################################################################
+'''
+Listings + Requirements
+'''
+class ListingProfile(models.Model):
+	is_active = models.BooleanField(default=False)
+	equipment = models.ForeignKey(_('Equipment'), EquipmentProfile)
+	price = models.DecimalField(_('Price'), max_digits=15, decimal_places=2)
+	post_date = models.DateField(_('Posted on'), auto_now=False, auto_now_add=False, blank=True)
+	description = models.TextField(_('Description'), blank=True)
+
+	def __unicode__(self):
+		return 'hello' # TODO
+
+class AbstractRequirement(models.Model):
+	features = JSONField(_('Feature Requirements'), blank=True, default='')
+	price = JSONField(_('Price Requirements'), blank=True, default='') # TODO: blank JSON Object
+	manufacture_date = JSONField(_('Manufacture Date Requirements'), blank=True, default='')
+
+	class Meta:
+        abstract = True
+
+class RequirementProfile(AbstractRequirement):
+	is_active = models.BooleanField(default=False)
+	contact = models.ForeignKey(_('Posted by'), ContactProfile)
+	trader = models.ForeignKey(_('Handled by'), TraderProfile)
+	post_date = models.DateField(_('Posted on'), auto_now=False, auto_now_add=False, blank=True)
+	description = models.TextField(_('Description'), blank=True)
+
+	def __unicode__(self):
+		return 'hello' # TODO
+
+class RequirementRecommendation(models.Model):
+	is_active = models.BooleanField(default=False)
+	requirement = models.ForeignKey(_('Requirement'), RequirementProfile)
+	core = models.ForeignKey(_('Make & Model'), EquipmentCore)
+	trader = models.ForeignKey(_('Proposed by'), TraderProfile)
+	description = models.TextField(_('Description'), blank=True)
+
+	def __unicode__(self):
+		return 'hello' # TODO
 
