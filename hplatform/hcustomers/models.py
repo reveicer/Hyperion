2872679cdd6635007708e240997661bea4e50c1c
Hongxia <hongxia.zhong@gmail.com>
2013-04-23 11:47:16 -0700

    completed reorg with updated models

---
+++ hplatform/hcustomers/models.py
@@ -0,0 +1,113 @@
+from django.db import models
+
+class Industry(models.Model):
+	is_active = models.BooleanField(default=False)
+	code = models.CharField('Code', max_length=3)
+	name = models.CharField('Name', max_length=50)
+
+class Category(models.Model):
+	is_active = models.BooleanField(default=False)
+	code = models.CharField('Code', max_length=3)
+	name = models.CharField('Name', max_length=50)
+
+class SubCategory(models.Model):
+	is_active = models.BooleanField(default=False)
+	code = models.CharField('Code', max_length=3)
+	name = models.CharField('Name', max_length=50)
+	category = models.ForeignKey(Category, related_name='subcategories')
+
+class Type(models.Model):
+	is_active = models.BooleanField(default=False)
+	code = models.CharField('Code', max_length=3)
+	name = models.CharField('Name', max_length=50)
+
+class Region(models.Model):
+	is_active = models.BooleanField(default=False)
+	code = models.CharField('Code', max_length=3)
+	name = models.CharField('Name', max_length=50)
+
+class CompanyProfile(models.Model):
+	is_active = models.BooleanField(default=False)
+	name = models.CharField('Name', max_length=50)
+	region = models.ForeignKey(Region)
+	primary_type = models.ForeignKey(Type, related_name='primary_company_profiles')
+	secondary_types = models.ManyToManyField(Type, related_name='secondary_company_profiles')
+	about = models.TextField('About', blank=True)
+	categories = models.ManyToManyField(Category, related_name='company_profiles')
+	subcategories = models.ManyToManyField(SubCategory, related_name='company_profiles')
+	industries = models.ManyToManyField(Industry, related_name='company_profiles')
+	website = models.URLField('Website', max_length=200)
+	email = models.EmailField('E-mail', blank=True)
+	phone = models.CharField('Phone', max_length=40)
+	fax = models.CharField('Fax', max_length=40)
+	street_line1 = models.CharField('Address 1', max_length=100, blank=True)
+	street_line2 = models.CharField('Address 2', max_length=100, blank=True)
+	city = models.CharField('City', max_length=100, blank=True)
+	county = models.CharField('County', max_length=100, blank=True)
+	state = models.CharField('State', max_length=100, blank=True)
+	province = models.CharField('Province', max_length=100, blank=True)
+	country = models.CharField('Country', max_length=100, blank=True)
+	zip_code = models.CharField('ZIP Code', max_length=20, blank=True)
+	notes = models.TextField('Notes', blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+class ContactProfile(models.Model):
+	is_active = models.BooleanField(default=False)
+	first_name = models.CharField('First Name', max_length=30, blank=True)
+	last_name = models.CharField('Last Name', max_length=30, blank=True)
+	region = models.ForeignKey(Region, related_name='contact_profiles')
+	primary_type = models.ForeignKey(Type, related_name='primary_contact_profiles')
+	secondary_types = models.ManyToManyField(Type, related_name='secondary_contact_profiles')
+	categories = models.ManyToManyField(Category, related_name='contact_profiles')
+	subcategories = models.ManyToManyField(SubCategory, related_name='contact_profiles')
+	email = models.EmailField('E-mail', blank=True)
+	company = models.ForeignKey(CompanyProfile, blank=True)
+	department = models.CharField('Department', max_length=20, blank=True)
+	title = models.CharField('Title', max_length=20, blank=True)
+	buy = models.BooleanField('Buys', default=False)
+	sell = models.BooleanField('Sells', default=False)
+	expertise = models.TextField('Expertise', blank=True)
+	notes = models.TextField('Notes', blank=True)
+
+	def get_full_name(self):        
+		return '%s %s' % (self.first_name, self.last_name)
+
+	def __unicode__(self):
+		return self.get_full_name()
+
+class ContactPhone(models.Model):
+	PHONE_TYPE_CHOICES = (
+		('HOME', 'Home'),
+		('WORK', 'Work'),
+		('MOBILE', 'Mobile'),
+		('FAX', 'Fax'),
+		('OTHER', 'Other'),
+	)
+
+	is_active = models.BooleanField(default=False)
+	contact = models.ForeignKey(ContactProfile, related_name='contact_phones')
+	phone_type = models.CharField('Type', max_length=10, choices=PHONE_TYPE_CHOICES, default='OTHER')
+	number = models.CharField('Number', max_length=40)
+	comment = models.TextField('Comment', blank=True)
+
+class ContactAddress(models.Model):
+	ADDRESS_TYPE_CHOICES = (
+		('CORP', 'Corporate'),
+		('MAIL', 'Mailing'),
+		('WORK', 'Work'),
+		('OTHER', 'Other'),
+	)
+
+	is_active = models.BooleanField(default=False)
+	contact = models.ForeignKey(ContactProfile, related_name='contact_addresses')
+	address_type = models.CharField('Type', max_length=10, choices=ADDRESS_TYPE_CHOICES, default='OTHER')
+	street_line1 = models.CharField('Address 1', max_length=100, blank=True)
+	street_line2 = models.CharField('Address 2', max_length=100, blank=True)
+	city = models.CharField('City', max_length=100, blank=True)
+	county = models.CharField('County', max_length=100, blank=True)
+	state = models.CharField('State', max_length=100, blank=True)
+	province = models.CharField('Province', max_length=100, blank=True)
+	country = models.CharField('Country', max_length=100, blank=True)
+	zip_code = models.CharField('ZIP Code', max_length=20, blank=True)
--- hplatform/hplatform/settings.py
+++ hplatform/hplatform/settings.py
@@ -1,21 +1,23 @@
 # Django settings for hplatform project.
+import os.path
+PROJECT_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Hongxia Zhong', 'hongxia.zhong@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hyperiondb',                      # Or path to database file if using sqlite3.
+        'USER': 'hyperiondev',
+        'PASSWORD': 'hyperionkey',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -29,7 +31,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -120,10 +122,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'htraders',
+    'hcustomers',
+    'hinventory',
 )
 
 # A sample logging configuration. The only tangible logging
