4683e383e6ed8c94cb87acb4706ac730f0577942
Hongxia <hongxia.zhong@gmail.com>
2013-04-24 03:45:23 -0700

    revised STATIC_ROOT

--- hplatform/hplatform/settings.py
+++ hplatform/hplatform/settings.py
@@ -65,7 +65,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''#path to files collected by collectstatic
+#STATIC_ROOT = ''#path to files collected by collectstatic
+STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
